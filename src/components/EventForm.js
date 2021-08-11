11ed3841d84cf8e814353ff7a407a18bb26c72a1
AtsushiHayasaka <a.hayusaka0614@icloud.com>
2021-08-11 09:18:57 +0900

    次から定数を定義する

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { CREATE_EVENT, DELETE_ALL_EVENTS } from '../actions';
 
 const EventForm = ({ state, dispatch }) => {
   const [title, setTitle] = useState('');
@@ -10,7 +11,7 @@ const EventForm = ({ state, dispatch }) => {
       return;
     }
     dispatch({
-      type: 'CREATE_EVENT',
+      type: CREATE_EVENT,
       title,
       body,
     });
@@ -27,7 +28,7 @@ const EventForm = ({ state, dispatch }) => {
     }
 
     dispatch({
-      type: 'DELETE_ALL_EVENTS',
+      type: DELETE_ALL_EVENTS,
     });
   };
 
